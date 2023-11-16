919aba9c9545819440e54c628f52c6c3247ff89f
issa1999 <issa.taleb@ensi-uma.tn>
2023-11-16 02:29:46 +0100

    code cleaning + adding static option existance check

---
+++ Options.hh
@@ -0,0 +1,64 @@
+#include<pair>
+#include <map>
+#include<string>
+static enum Tools {VELPREP, VELPOST, OTHER};
+class Option {
+    public:
+    Option() ;
+    Option(Option&);
+    static bool exists(Option& option); // check if the given Option exists in _Options of class Options
+    //enum OptionStatus {MANDATORY, OPTIONAL};
+    enum OptionType {BOOLEAN, STRING, NUMBER, REAL, OTHER, NONE};
+    void supportMultipleValues() {_haveMultipleValues = true;}
+    bool haveMultipleValues() {return _haveMultipleValues;  }
+    const std::string& getOptionName() {return _optionName;}
+    OptionType getType() {return _type;}
+    bool isMandatory() { return _isMandatory;}
+    void setOptionValue(int index, char*  value) {_value.insert(make_pair<int, char*>(index, value));}
+    const std::string& getHelp() { return _description;}
+    std::map<int, char*> getValue() {return _value;}
+
+    friend bool operator==(const Option& l, const Option& r);
+    private:
+    bool _haveMultipleValues;
+    std::string _optionName;
+    std::map<int, char*> _value;
+    OptionType _type;
+    bool _isMandatory;
+    std::string _description;
+};
+const Option NullOption();
+class Options {
+    friend Option;
+    public:
+    int computeOptions(int argc);
+    static bool matchesTool(Option option, Tools tool);
+    bool canAddOption(Option option);
+    void addNewOption(Tools tool, Option option);
+    void registerCommand(
+        Option::OptionType type,
+        const std::string& name,
+        bool multipleValues,
+        bool isMandatory,
+        std::string description,
+        Tools tool
+    );
+    void displayHelp(std::string tool);
+
+    private:
+    std::map<int, Option> _Options;
+    static std::map<Tools, Options> _ToolsOptions;
+};
+class ArgumentParser {
+    friend Option;
+    friend Options;
+    public:
+    void setTool(Tools tool) {_tool = tool;}
+    Tools getTool() {return _tool;}
+    computeOptions(int argc);
+    int execute(int argc, char* argv[]);
+    void parseArguments(int argc, char* argv[]);
+    private:
+    int argv;
+    char* argc[];
+};
